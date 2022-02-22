16d0e62702e159284f397b3e59ec16406eac4c31
cpoonolly <rpoonolly@gmail.com>
2022-02-21 18:37:24 -0800

    Moving deployment to package.json

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-gcloud functions deploy guess_the_dj__play --entry-point play --runtime nodejs16 --trigger-http --allow-unauthenticated &&
-gcloud functions deploy guess_the_dj__submit --entry-point submit --runtime nodejs16 --trigger-http --allow-unauthenticated &&
-gcloud functions deploy guess_the_dj__guess --entry-point guess --runtime nodejs16 --trigger-http --allow-unauthenticated &&
-gcloud functions deploy guess_the_dj__reveal --entry-point reveal --runtime nodejs16 --trigger-http --allow-unauthenticated
